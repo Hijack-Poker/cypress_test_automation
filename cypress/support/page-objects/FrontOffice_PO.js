e955028244aaffbcb438dc01bb89f41ffa4796c7
Alfe Nino Lana <alana@oppy.tech>
2024-01-25 22:41:14 +0800

    EN-2787 initial commit

--- cypress/support/page-objects/FrontOffice_PO.js
+++ cypress/support/page-objects/FrontOffice_PO.js
@@ -13,43 +13,30 @@ class FrontOffice_PO {
       /// Click Login Email button
       cy.get('descope-wc[project-id="P2TjFGLikGRSHKrrxgAHf2o3cM7w"]')
         .shadow()
-        .find("#KAG9NgACSP")
+        .find("#bsRZVUo8ym")
         .shadow()
-        .find("#KAG9NgACSP")
+        .find("#bsRZVUo8ym")
+        .shadow()
+        .find(".vaadin-button-container")
         .click();
-
+    });
+    cy.origin("https://accounts.google.com", () => {
       /// Input email address
-      cy.get('descope-wc[project-id="P2TjFGLikGRSHKrrxgAHf2o3cM7w"]')
-        .shadow()
-        .find("#GIhmN0AQ39")
-        .shadow()
-        .find("#input-vaadin-email-field-3")
-        .type("jpanares+2@oppy.tech");
+      cy.get("#identifierId").type("alana@oppy.tech{enter}");
 
       /// Input password
-      cy.get('descope-wc[project-id="P2TjFGLikGRSHKrrxgAHf2o3cM7w"]')
-        .shadow()
-        .find("#b2gUaVb8UG")
-        .shadow()
-        .find("#input-vaadin-password-field-7")
-        .type("Admin123");
-
-      /// Click Login button
-      cy.get('descope-wc[project-id="P2TjFGLikGRSHKrrxgAHf2o3cM7w"]')
-        .shadow()
-        .find("#VaCr2LiM1K")
-        .shadow()
-        .find("#VaCr2LiM1K")
-        .click();
+      cy.get("[jscontroller='bzud8b'] [jsname='YPqjbf']").type("Tw1tt3rC@rb0n{enter}");
     });
   }
 
   clickProfile() {
-    cy.get("#navbarDropdownUserImage").click();
+    cy.get("#a#navbarDropdownUserImage > img").should("exist").click();
   }
 
   clickLogoutBtn() {
-    cy.get("[aria-labelledby] .dropdown-item:nth-child(4)").click();
+    cy.get(
+      ".animated--fade-in-up.border-0.dropdown-menu.dropdown-menu-right.shadow.show > a:nth-of-type(2)"
+    ).click();
   }
 
   reEnterUrl() {
@@ -65,13 +52,12 @@ class FrontOffice_PO {
       cy.get('descope-wc[project-id="P2TjFGLikGRSHKrrxgAHf2o3cM7w"]')
         .shadow()
         .find("#tIhFQOC4fm")
-        .should("be.visible")
-        .and("exist");
+        .should("exist");
     });
   }
 
   verifyIconIsDisplayed() {
-    cy.get("img.logo").should("be.visible");
+    cy.get(".logo").should("be.visible");
   }
 
   clickLoginEmailButton() {
@@ -128,6 +114,7 @@ class FrontOffice_PO {
         .find("#b2gUaVb8UG")
         .shadow()
         .find("#input-vaadin-password-field-7")
+        .should("exist")
         .type("invalidPassw0rd");
     });
   }
@@ -144,13 +131,13 @@ class FrontOffice_PO {
   verifyInvalidCredentialsMessageDisplayed() {
     cy.origin("https://auth.descope.io", () => {
       const cssSelectorForHost1 =
-        "descope-wc[project-id='P2TjFGLikGRSHKrrxgAHf2o3cM7w']";
+        'descope-wc[project-id="P2TjFGLikGRSHKrrxgAHf2o3cM7w"]';
       cy.get(cssSelectorForHost1)
         .shadow()
         .find(
           "div:nth-child(2) > descope-container:nth-child(1) > descope-text:nth-child(7)"
         )
-        .should("be.visible");
+        .should("exist");
     });
   }
 
@@ -170,11 +157,11 @@ class FrontOffice_PO {
   }
 
   clickNameTxtField(fieldValue) {
-    if (fieldValue.includes('Display Name')) {
+    if (fieldValue.includes("Display Name")) {
       cy.get("a#editdisplayname").click();
-    } else if (fieldValue.includes('First Name')) {
+    } else if (fieldValue.includes("First Name")) {
       cy.get("a#editfirstname").click();
-    } else if (fieldValue.includes('Last Name')) {
+    } else if (fieldValue.includes("Last Name")) {
       cy.get("a#editlastname").click();
     } else {
       //blank for now
@@ -182,11 +169,11 @@ class FrontOffice_PO {
   }
 
   clearNameTxtField(fieldValue) {
-    if (fieldValue.includes('Display Name')) {
+    if (fieldValue.includes("Display Name")) {
       cy.get("input#displayname").clear();
-    } else if (fieldValue.includes('First Name')) {
+    } else if (fieldValue.includes("First Name")) {
       cy.get("input#firstname").clear();
-    } else if (fieldValue.includes('Last Name')) {
+    } else if (fieldValue.includes("Last Name")) {
       cy.get("input#lastname").clear();
     } else {
       //blank for now
@@ -194,11 +181,11 @@ class FrontOffice_PO {
   }
 
   clickSaveButton(fieldValue) {
-    if (fieldValue.includes('Display Name')) {
+    if (fieldValue.includes("Display Name")) {
       cy.get("button#submitdisplayname").click();
-    } else if (fieldValue.includes('First Name')) {
+    } else if (fieldValue.includes("First Name")) {
       cy.get("button#submitfirstname").click();
-    } else if (fieldValue.includes('Last Name')) {
+    } else if (fieldValue.includes("Last Name")) {
       cy.get("button#submitlastname").click();
     } else {
       //blank for now
@@ -213,13 +200,13 @@ class FrontOffice_PO {
   }
 
   editNameField(fieldValue, textValue) {
-    if (fieldValue.includes('Display Name')) {
+    if (fieldValue.includes("Display Name")) {
       cy.get("input#displayname").clear();
       cy.get("input#displayname").type(textValue);
-    } else if (fieldValue.includes('First Name')) {
+    } else if (fieldValue.includes("First Name")) {
       cy.get("input#firstname").clear();
       cy.get("input#firstname").type(textValue);
-    } else if (fieldValue.includes('Last Name')) {
+    } else if (fieldValue.includes("Last Name")) {
       cy.get("input#lastname").clear();
       cy.get("input#lastname").type(textValue);
     } else {
--- cypress/support/step_definitions/FrontOfficePage_Steps.js
+++ cypress/support/step_definitions/FrontOfficePage_Steps.js
@@ -1,92 +1,95 @@
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 import FrontOffice_PO from "../page-objects/FrontOffice_PO";
 
-const frontOfficePage = new FrontOffice_PO();
 
+const frontOfficePage = new FrontOffice_PO();
 
 // Action methods
 Given("I login in the HiJack Poker", () => {
-    frontOfficePage.navigate();
+  frontOfficePage.navigate();
 });
 
 When("I logged in using the testing account", () => {
-    frontOfficePage.clickLoginDetails();
+  frontOfficePage.clickLoginDetails();
 });
 
 When("I click my profile", () => {
-    frontOfficePage.clickProfile();
+  frontOfficePage.clickProfile();
 });
 
 When("I click the logout button", () => {
-    frontOfficePage.clickLogoutBtn();
+  frontOfficePage.clickLogoutBtn();
 });
 
 When("I access the website by manually entering the URL", () => {
-    frontOfficePage.reEnterUrl();
+  frontOfficePage.reEnterUrl();
 });
 
 When("I input the test email in the login email", () => {
-    frontOfficePage.inputEmailAddress();
+  frontOfficePage.inputEmailAddress();
 });
 
 When("I click the forget password", () => {
-    frontOfficePage.clickForgetPasswordBtn();
-})
+  frontOfficePage.clickForgetPasswordBtn();
+});
 
 When("I click the Login with Email option", () => {
-    frontOfficePage.clickLoginEmailButton();
-})
+  frontOfficePage.clickLoginEmailButton();
+});
 
 When("I input invalid email for login", () => {
-    frontOfficePage.inputInvalidEmailAddress();
-})
+  frontOfficePage.inputInvalidEmailAddress();
+});
 
 When("I input invalid password", () => {
-    frontOfficePage.inputInvalidPassword();
-})
+  frontOfficePage.inputInvalidPassword();
+});
 
 When("I click the login button", () => {
-    frontOfficePage.clickLoginBtn();
-})
+  frontOfficePage.clickLoginBtn();
+});
 
 When("I click the edit {string} on Account Details", (fieldValue) => {
-    frontOfficePage.clickNameTxtField(fieldValue);
-})
+  frontOfficePage.clickNameTxtField(fieldValue);
+});
 
 When("I empty the name field of {string}", (boxValue) => {
-    frontOfficePage.clearNameTxtField(boxValue);
-})
+  frontOfficePage.clearNameTxtField(boxValue);
+});
 
 When("I click the Save button of {string} on account details", (btnValue) => {
-    frontOfficePage.clickSaveButton(btnValue);
-})
+  frontOfficePage.clickSaveButton(btnValue);
+});
 
 When("I click the Account button", () => {
-    frontOfficePage.clickAccountBtn();
-})
+  frontOfficePage.clickAccountBtn();
+});
 
-When("I edit the {string} to {string} on Account Details", (fieldValue, textValue) => {
+When(
+  "I edit the {string} to {string} on Account Details",
+  (fieldValue, textValue) => {
     frontOfficePage.editNameField(fieldValue, textValue);
-})
+  }
+);
 
 // Verification methods
 
 Then("The login page HiJack will be displayed", () => {
-    frontOfficePage.verifyTheWelcomeMessage();
+  frontOfficePage.verifyTheWelcomeMessage();
 });
 
 Then("The HiJack Logo header should be displayed", () => {
-    frontOfficePage.verifyIconIsDisplayed();
-})
+  frontOfficePage.verifyIconIsDisplayed();
+});
 
 Then("Reset password modal should be displayed", () => {
-    frontOfficePage.verifyResetModalIsDisplayed();
-})
+  frontOfficePage.verifyResetModalIsDisplayed();
+});
 
 Then("The login error message should be displayed", () => {
-    frontOfficePage.verifyInvalidCredentialsMessageDisplayed();
-})
+  frontOfficePage.verifyInvalidCredentialsMessageDisplayed();
+});
 
 Then("The error message {string} should be displayed", (errorValue) => {
-    frontOfficePage.verifyErrorMsg(errorValue);
-})
+  frontOfficePage.verifyErrorMsg(errorValue);
+});
