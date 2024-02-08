9986ca0af32763d63642bd09d34164df3222d871
Anthony <amarmansueto@gmail.com>
2024-02-08 23:19:13 +0800

    updated

--- cypress/support/step-definitions/ui/front-office/AccountSettingsPage_Steps.js
+++ cypress/support/step-definitions/ui/front-office/AccountSettingsPage_Steps.js
@@ -1,8 +1,6 @@
 import frontOfficeLocators from "../../../element-locators/front-office-locators";
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-let avatar_profile;
-
 When('I click on Go to Help Portal link in Account Settings - Help page', () => {
   cy.get(frontOfficeLocators.account_settings_page.go_to_help_portal_link).invoke('removeAttr','target').click();
 });
@@ -10,13 +8,13 @@ When('I click on Go to Help Portal link in Account Settings - Help page', () => 
 When('I select an avatar from the Avatar Selection section', () => {
   cy.get(frontOfficeLocators.navigation_bar.avatar_button).invoke('attr','src').then((srcValue) => {
     let index = 0;
-    let avatar_image = 'bank';
-    if (srcValue.includes('saloon')) {
+    let avatar_image = 'saloon';
+    if (srcValue.includes('bank')) {
       index = 1;
-      avatar_image = 'saloon';
+      avatar_image = 'bank';
     }
     cy.get(frontOfficeLocators.account_settings_page.avatar_images).eq(index).click();
-    avatar_profile = avatar_image;
+    this.avatar_profile = avatar_image;
   });
   cy.get(frontOfficeLocators.account_settings_page.avatar_updated_modal).find('#ModalBody').should('be.visible').and('contain','Avatar Updated!');
   cy.get(frontOfficeLocators.account_settings_page.avatar_updated_modal).contains('Close').click();
@@ -25,5 +23,11 @@ When('I select an avatar from the Avatar Selection section', () => {
 Then('HiJack Help Portal is displayed with url {string}', (url)=> {
   cy.origin("https://hijack.freshdesk.com/support/solutions", { args: {url}} ,({url}) => {
     cy.url().should('include', url);
+  });
+});
+
+Then('New Avatar is displayed in my Account Profile', ()=> {
+  cy.get(frontOfficeLocators.navigation_bar.avatar_button).invoke('attr','src').then((srcValue) => {
+    expect(srcValue).includes(this.avatar_profile);
   });
 });
