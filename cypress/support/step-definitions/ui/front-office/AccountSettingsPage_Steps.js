e736cb6be1231e954bb3a0e664885df7967804d2
Anthony <amarmansueto@gmail.com>
2024-02-16 13:39:21 +0800

    fix intermittent2

--- cypress/support/step-definitions/ui/front-office/AccountSettingsPage_Steps.js
+++ cypress/support/step-definitions/ui/front-office/AccountSettingsPage_Steps.js
@@ -37,7 +37,7 @@ When('I edit the Account Details fields with these values', (dataTable) => {
       cy.get(inputTextbox).clear().type(fieldValue);
       cy.get(saveButton).should('be.visible').click();
       cy.get(frontOfficeLocators.common.message_modal).find('#ModalBody').should('be.visible');
-      cy.get(frontOfficeLocators.common.message_modal).contains('×').click();
+      cy.get(frontOfficeLocators.common.message_modal).should('be.visible').contains('×').click();
     });
   });
 });
